be5c0c5ee0bfd5a5088a8c352cb942bb1b2fa255
Eugene <Eugene Crystal>
2020-11-19 17:50:37 +0300

    Add normal equation

--- gradient-descent-and-normal-equations/computeCost.m
+++ gradient-descent-and-normal-equations/computeCost.m
@@ -1,5 +1,3 @@
-% Single Variable Gradient Descent
-
 function J = computeCost(X, y, theta)
 %COMPUTECOST Compute cost for linear regression
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
@@ -18,7 +16,10 @@ function J_growth = computeCostGrowth(X, y, theta, feature_num)
 %   with specified theta parameters and current feature number
 
 m = length(y);
-J_growth = (((X * theta) - y)' * X(:, feature_num)) / m;
+h = X * theta; % h - for hypothesis, estimate;
+e = h - y; % e - for error;
+
+J_growth = (e' * X(:, feature_num)) / m;
 
 end
 
@@ -54,18 +55,23 @@ end
 
 
 function J_growth = computeCostGrowthMulti(X, y, theta)
-%COMPUTECOSTGROWTH Compute growth from computeCost function's derivative
-%   with specified theta parameters and current feature number
+%COMPUTECOSTGROWTHMULTI Compute growth from computeCost function's derivative
+%   with specified theta parameters but without multiplying by features variable
+%   as it'll be multiplied by matrix of them (X) and assigned to all thetas
+%   accordingly
 
 m = length(y);
-J_growth = ((X * theta) - y) / m;
+h = X * theta; 
+e = h - y; 
+
+J_growth = e / m;
 
 end
 
 
 
 function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
+%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
@@ -80,17 +86,4 @@ for iter = 1:num_iters
 end
 
 
-
-function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
-%   FEATURENORMALIZE(X) returns a normalized version of X where
-%   the mean value of each feature is 0 and the standard deviation
-%   is 1. This is often a good preprocessing step to do when
-%   working with learning algorithms.
-
-mu = mean(X);
-sigma = std(X);
-X_norm = (X - mu) ./ sigma;
-
-end
 
